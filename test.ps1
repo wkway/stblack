07bf74999cf33c0528c4830afcab286c16898f07
wkway <104142595+wkway@users.noreply.github.com>
2022-05-10 01:19:12 -0700

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,11 +1,7 @@
 echo '' >myhosts1
-echo '##########################################' >>myhosts1
-echo '######           GithubHosts       #######' >>myhosts1
-echo '##########################################' >>myhosts1
-echo '' >>myhosts1
-Invoke-WebRequest -UseBasicParsing -Uri "https://raw.hellogithub.com/hosts" -outfile 1
+Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/521xueweihan/GitHub520/main/hosts" -outfile 1
 type 1 >>myhosts1
-Invoke-WebRequest -UseBasicParsing -Uri "https://gitee.com/ineo6/hosts/raw/master/next-hosts" -outfile 2
+Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/ineo6/hosts/master/hosts" -outfile 2
 type 2 >>myhosts1
 Get-Content myhosts1 | Out-File myhosts -Encoding ascii
 Remove-Item 1
